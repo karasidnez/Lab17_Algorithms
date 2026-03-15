38035d43d93dcf0a2c3c3b38806de28049c7bca7
karasidnez <karasdnez@gmail.com>
2026-03-15 14:04:04 +0300

    Step 7 arrays

---
+++ code/step7_arrayElement.cs
@@ -0,0 +1,7 @@
+int[] array = { 5, 2, 8, 1, 9, 3 };
+        Console.WriteLine("Элементы массива:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine(); 
---
+++ code/step7_arrayMax.cs
@@ -0,0 +1,10 @@
+int[] array = { 5, 2, 8, 1, 9, 3 };
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine(max);
